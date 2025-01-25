60b5dae28d19133ce033f4592a25ad2fb12d43ad
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2025-01-25 22:28:05 +0600

    Add login and registration pages, and implement MongoDB connection utility

---
+++ util/db.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = "mongodb+srv://gadget-storebd:WJqb12kP4tHYGEB2@cluster0.nswkl.mongodb.net/event management?retryWrites=true&w=majority"
+
+const cached = {};
+async function connectMongo() {
+    if (!MONGO_URI) {
+        throw new Error('Please define the MONGO_URI environment variable inside .env.local');
+    }
+    if (cached.connection) {
+        return cached.connection;
+    }
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+        cached.promise = mongoose.connect(MONGO_URI, opts);
+    }
+    try {
+        cached.connection = await cached.promise;
+    } catch (e) {
+        cached.promise = undefined;
+        throw e;
+    }
+    return cached.connection;
+}
+export default connectMongo;
