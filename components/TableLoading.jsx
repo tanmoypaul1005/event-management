5125be0897b7ff304d563d61dde4132aa676f5fd
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2025-01-29 00:27:59 +0600

    Update React and ReactDOM to version 19.0.0; comment out unused RotatingLines import in TableLoading component

--- components/TableLoading.jsx
+++ components/TableLoading.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { RotatingLines } from "react-loader-spinner";
+// import { RotatingLines } from "react-loader-spinner";
 function TableLoading() {
     return (
         <tr className="container w-full h-[50vh]">
             <td className='' colSpan="6">
                 <div className='flex justify-center items-center'>
 
-                <RotatingLines
+                {/* <RotatingLines
                     width="100"
                     strokeColor="#285D43"
                     strokeWidth={4}
                     strokeWidthSecondary={3}
-                    />
+                    /> */}
                     </div>
             </td>
         </tr>
