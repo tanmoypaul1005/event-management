946f83f764e2422c2498ed8aabb5522bf1c7e9df
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2025-01-29 00:09:58 +0600

    Add TableLoading component with spinner; update package.json to include react-loader-spinner

---
+++ components/TableLoading.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { RotatingLines } from "react-loader-spinner";
+function TableLoading() {
+    return (
+        <tr className="container w-full h-[40vh]">
+            <td className='' colSpan="6">
+                <div className='flex justify-center items-center'>
+
+                <RotatingLines
+                    width="100"
+                    strokeColor="#285D43"
+                    strokeWidth={4}
+                    strokeWidthSecondary={3}
+                    />
+                    </div>
+            </td>
+        </tr>
+
+
+    )
+}
+
+export default TableLoading
